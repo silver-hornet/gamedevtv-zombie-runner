0b71b44898505619e4fe93fac17e8cdc0c32df52
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-11-21 10:10:38 -0500

    Added Unity Standard Assets FPS player controller, prototype objects, enemy, enemy AI, and adjusted player head bob and friction

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,7 +6,12 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    // How far is the enemy from the target? Initialized at a gigantic number, otherwise it would be initialized at 0 by default,
+    // which would make the enemy think that it's right on top of the player.
 
     void Start()
     {
@@ -15,6 +20,11 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (distanceToTarget <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
     }
 }
